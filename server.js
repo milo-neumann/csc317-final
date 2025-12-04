9e645c6f9e2f060a97eb1e7c770c5d4c4aa70d76
Morgan <morgan@Barad-dr.localdomain>
2025-12-03 21:53:51 -0800

    started implemntation of database and price seeding

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-
+const db = require(path.join(__dirname, "data", "database.js"));
+const seed = require(path.join(__dirname, "data", "seed.js"));
 const PORT = process.env.PORT || 3000;
 
 // Serve everything in /public as static assets
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 // (Optional) Explicit route for home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// route to fill out the price database
+app.get("/reseed", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "reseed.html"));
+});
+app.post("/api/reseed", (req, res) => {
+  const rows = seed();
+  res.json(rows);
 });
 
 app.listen(PORT, () => {
